f8e852be658583520ca6f6587285c30f3f7f95db
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-26 21:11:18 +0800

    Component constructor

--- analysis/createComponent.js
+++ analysis/createComponent.js
@@ -12,4 +12,26 @@ function createComponent(Ctor, data, context, children, tag) {
 /**
  * Class inheritance
  */
+function extend(extendOptions) {
+    extendOptions = extendOptions || {};
+    const Super = this;
+    const SuperId = Super.cid;
+    const cachedCtors = extendOptions._Ctor;
+    
+    const Sub = function VueComponent(options) {
+        this._init(options);
+    }
+    Sub.prototype = o(Super.prototype);
+    Sub.prototype.constructor = Sub;
 
+    Sub.options = mergeOptions(/** */);
+
+    // ... add some global static properties
+    cachedCtors[SuperId] = Sub;
+    return Sub;
+}
+function o(obj) {
+    function F() {}
+    F.prototype = obj;
+    return new F();
+}
