266ce4611b9c68166d019c257a3810850c24e814
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-26 18:52:30 +0800

    component analysis start

--- test/vue-test/src/App.vue
+++ test/vue-test/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app"></div>
+  <div id="app"> {{ message }} </div>
 </template>
 
 <script>
--- test/vue-test/src/main.js
+++ test/vue-test/src/main.js
@@ -1,21 +1,9 @@
 import Vue from 'vue'
-// import App from './App.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
-  // render: h => h(App),
-
-  render: function(createElement) {
-    return createElement('div', {
-      attrs: {
-        id: 'app',
-      },
-    }, this.msg)
-  },
-
-  data: {
-    msg: 'vueeeeee'
-  }
+  render: h => h(App),
 })
