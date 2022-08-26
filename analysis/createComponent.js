947bf06cd62e60d754e35f106e51ac33f427cf4e
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-26 21:19:47 +0800

    create vnode

--- analysis/createComponent.js
+++ analysis/createComponent.js
@@ -1,12 +1,22 @@
 
 
 function createComponent(Ctor, data, context, children, tag) {
+    // 需要一个继承了Vue Ctor的构造器，来持有创建组件的能力
+    // 我们的组件构造器实际上就是以Vue为模板的
     const baseCtor = context.$options._base;
     Ctor = baseCtor.extend(Ctor);
 
+    // patch 阶段需要各种各样的钩子
     installComponentHooks(data);
-
-    const vnode = new VNode();
+    
+    const name = Ctor.options.name;
+    // 注意组件VNode的children为undefined，而componentOptions有children属性
+    const vnode = new VNode(
+        `vue-component-${Ctor.cid}${name ? `-${name}` : ''}`,
+        data, undefined, undefined, undefined, context,
+        { Ctor, propsData, listeners, tag, children },
+        asyncFactory
+    );
     return vnode;
 }
 /**
