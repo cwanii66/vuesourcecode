ce191bc2f1a7ee00978661d55dca3d65edec9cdc
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-01 19:46:08 +0800

    example for mergeOption

--- test/vue-test/src/main.js
+++ test/vue-test/src/main.js
@@ -1,9 +1,30 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
 Vue.config.productionTip = false
 
+const childComp = {
+  template: '<div>{{ msg }}</div>',
+  created() {
+    console.log('child created');
+  },
+  mounted() {
+    console.log('child mounted');
+  },
+  data() {
+    return {
+      msg: 'hello vue...'
+    }
+  }
+}
+
+Vue.mixin({
+  created() {
+    console.log('parent created');
+  }
+})
+
 new Vue({
   el: '#app',
-  render: h => h(App),
+  render: h => h(childComp),
 })
