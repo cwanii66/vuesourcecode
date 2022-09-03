17b560e63a475dbba837faad4d5b853cf0ddb8af
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-03 10:41:20 +0800

    execution order of beforeMount & mounted

--- test/vue-test/src/main.js
+++ test/vue-test/src/main.js
@@ -8,6 +8,9 @@ const childComp = { // merge后为子类构造器的options
   created() {
     console.log('child created');
   },
+  beforeMount() {
+    console.log('childComp: beforeMount hook');
+  },
   mounted() {
     console.log('child mounted');
   },
@@ -19,8 +22,14 @@ const childComp = { // merge后为子类构造器的options
 } // 实例化子组件之前，在子组件的构造器内完成组件实例继承关系以及选项合并
 
 Vue.mixin({
+  beforeMount() {
+    console.log('global beforeMount')
+  },
   created() {
     console.log('parent created');
+  },
+  mounted() {
+    console.log('global mounted')
   }
 }) // 合并我们全局的options
 
