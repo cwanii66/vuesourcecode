94cb7916093a0d9568ff2807b6142357c5aabe8d
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-04 12:11:43 +0800

    component register

--- test/vue-test/src/main.js
+++ test/vue-test/src/main.js
@@ -1,39 +1,11 @@
 import Vue from 'vue'
-// import App from './App.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
-const childComp = { // merge后为子类构造器的options
-  render: h => h('div', { id: 'child' }, 'child component'),
-  created() {
-    console.log('child created');
-  },
-  beforeMount() {
-    console.log('childComp: beforeMount hook');
-  },
-  mounted() {
-    console.log('child mounted');
-  },
-  data() {
-    return {
-      msg: 'hello vue...'
-    }
-  }
-} // 实例化子组件之前，在子组件的构造器内完成组件实例继承关系以及选项合并
-
-Vue.mixin({
-  beforeMount() {
-    console.log('global beforeMount')
-  },
-  created() {
-    console.log('parent created');
-  },
-  mounted() {
-    console.log('global mounted')
-  }
-}) // 合并我们全局的options
+Vue.component('App', App)
 
 new Vue({
   el: '#app',
-  render: h => h(childComp),
+  render: h => h('App', null, null)
 })
