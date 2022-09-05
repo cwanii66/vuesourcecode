c19f62b062b080b1c48bcf10803060e3bb16f771
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-05 10:29:10 +0800

    async component demo1

--- test/vue-test/src/main.js
+++ test/vue-test/src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-Vue.component('App', App)
+Vue.component('HelloWorld', function(resolve) {
+  // require => 告诉webpack自动将编译后的代码分割成不同的块
+  require(['./components/HelloWorld.vue'], function(res) {
+    resolve(res)
+  })
+})
 
 new Vue({
   el: '#app',
-  render: h => h('App', null, null)
+  render: h => h(App)
 })
