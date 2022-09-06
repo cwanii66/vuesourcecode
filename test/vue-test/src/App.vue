167ddde5cd6c29c9cf118086b839d2c4a2226080
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-06 21:30:58 +0800

    reactive demo

--- test/vue-test/src/App.vue
+++ test/vue-test/src/App.vue
@@ -1,30 +1,37 @@
 <template>
   <div id="app">
-    <h1>{{ message }}</h1>
+    <h1 @click="changeMsg"> {{ message }} </h1>
     <HelloWorld />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
-  // components: {
-  //   HelloWorld,
-  // },
+  components: {
+    HelloWorld,
+  },
 
   data() {
     return {
       message: 'hello vue'
     }
   },
-  mounted() {
-    console.log(this.message);
+  methods: {
+    changeMsg() {
+      this.message = 'hello vue3'
+    }
   }
 }
 </script>
 
 <style>
-
+h1:active {
+  color: crimson;
+}
+h1:hover {
+  cursor: pointer;
+}
 </style>
