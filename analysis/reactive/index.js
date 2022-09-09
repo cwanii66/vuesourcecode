8ddc4605796f5ecaaa5e9cf2d1017f38af027ea8
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-09 15:14:12 +0800

    set getter & setter

--- analysis/reactive/index.js
+++ analysis/reactive/index.js
@@ -1,6 +1,29 @@
-let activeEffect
+/**
+ * 执行代码时产生side effect使程序的状态发生改变
+ * 状态的改变取决于副作用的依赖，所以effect们是他们依赖的订阅者
+ * 每当依赖变化是产生发起通知，产生side effect
+ * 所以 我们需要一个函数能在依赖变化时产生作用：
+ * 1. 变量被读取时会被追踪
+ * 2. 被读取，将该副作用设为变量的订阅者
+ * 3. 探测变化，通知订阅者重新执行副作用
+ */
+
+let activeEffect // we need to know which effect should be a subscriber
 class Dep {
-   subscribers = new Set() // side effect database
+   constructor(value) {
+      this.subscribers = new Set() // side effect database
+      this._value = value
+   }
+
+   get value() {
+      this.depend()
+      return this._value
+   }
+
+   set value(newValue) {
+      this._value = newValue
+      this.notify()
+   }
 
    depend() {
       if (activeEffect) {
@@ -21,11 +44,12 @@ function watchEffect(effect) {
    _effect()
 }
 
-const dep = new Dep()
+const ok = new Dep(true)
+const msg = new Dep('deps value')
 
 watchEffect(() => {
-   dep.depend() // track
-   console.log('effect run...')
-}) // effect run
-
-dep.notify() // effect run (trigger)
+   if (ok.value) {
+      console.log(msg.value)
+   }
+})
+msg.value = 'value changes'
