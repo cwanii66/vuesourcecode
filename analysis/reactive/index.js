ff3b7f6b19d2fc701f8c89aee8686cd65d45ada3
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-09 14:30:58 +0800

    basic mode of effect

--- analysis/reactive/index.js
+++ analysis/reactive/index.js
@@ -1,6 +1,31 @@
+let activeEffect
 class Dep {
-   constructor() {
+   subscribers = new Set() // side effect database
 
+   depend() {
+      if (activeEffect) {
+         this.subscribers.add(activeEffect)
+      }
    }
-    
+   notify() {
+      this.subscribers.forEach(effect => effect())
+   }
 }
+
+function watchEffect(effect) {
+   const _effect = () => {
+      activeEffect = _effect
+      effect()
+      activeEffect = null
+   }
+   _effect()
+}
+
+const dep = new Dep()
+
+watchEffect(() => {
+   dep.depend() // track
+   console.log('effect run...')
+}) // effect run
+
+dep.notify() // effect run (trigger)
