92fb98b6f58515c12884da5debd3c98fd1b0f916
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-18 11:36:35 +0800

    createComputedGetter(proxy mode)

--- analysis/reactive/src/computed.js
+++ analysis/reactive/src/computed.js
@@ -17,7 +17,7 @@ function defineComputed(target, key, userDef) {
             ? shouldCache
                 ? createComputedGetter(key)
                 : userDef.get
-                : createGetterInvoker(userDef.get)
+            : createGetterInvoker(userDef.get)
         sharedPropertyDefinition.set = userDef.set || function() {}
     }
     Object.defineProperty(target, key, sharedPropertyDefinition)
@@ -25,5 +25,16 @@ function defineComputed(target, key, userDef) {
 function createGetterInvoker(getter) {
     return function computedGetter() {
         getter.call(this, this)
+    }
+}
+// argument: user defined key
+// like a proxy
+function createComputedGetter(key) {
+    return function computedGetter() {
+        const watcher = this._computedWatchers && this._computedWatchers[key]
+        if (watcher) {
+            watcher.depend()
+            return watcher.evaluate()
+        }
     }
 }
