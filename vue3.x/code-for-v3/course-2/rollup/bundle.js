e20307dfe345cf2416df065177e2eb22b8c2640c
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-09-22 14:55:45 +0800

    rollup intro

---
+++ vue3.x/code-for-v3/course-2/rollup/bundle.js
@@ -0,0 +1,5 @@
+function foo(obj) {
+    obj && obj.foo;
+}
+
+foo();
---
+++ vue3.x/code-for-v3/course-2/rollup/rollup.config.js
@@ -0,0 +1,8 @@
+const config = {
+    input: 'input.js',
+    output: {
+        file: 'output.js',
+        format: 'iife'
+    }
+}
+export default config
